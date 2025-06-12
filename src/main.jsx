e6c2ed186409d676735128c28e0f8059245eb35e
Mcnick <82217105+Mcnick1337@users.noreply.github.com>
2025-06-12 02:07:29 +0200

    e

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+// File: src/main.jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <-- ADDED THIS LINE
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 👇 WRAPPED APP WITH BROWSERROUTER 👇 */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
